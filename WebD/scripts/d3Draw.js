26ed6ec26f04f34d181ae0e967de10fdd2561432
cfframe <3910544+cfframe@users.noreply.github.com>
2020-10-22 16:45:55 +0100

    Change sample data - wider geographical area. Use example map long and lat dimensions as basis for scales. Relate circle size to population size.

--- WebD/scripts/d3Draw.js
+++ WebD/scripts/d3Draw.js
@@ -1,50 +1,107 @@
 const DataSource = "sampleData/10Towns.json";
 // http://35.211.183.112/Circles/Towns/50
 
-var dataset = 
-[
-    {
-        "lng": -4.42065,
-        "Town": "Abernant",
-        "Population": 77356,
-        "County": "Carmarthenshire",
-        "lat": 51.88513
-    },
-    {
-        "lng": -3.26466,
-        "Town": "Abertridwr",
-        "Population": 51055,
-        "County": "Caerphilly",
-        "lat": 51.59752
-    },
-    {
-        "lng": -3.61599,
-        "Town": "Abbotskerswell",
-        "Population": 67099,
-        "County": "Devon",
-        "lat": 50.50507
-    }
-];
+var dataset =
+    [
+        {
+            "lng": -6.652,
+            "Town": "Aghadowey",
+            "Population": 48700,
+            "County": "Londonderry",
+            "lat": 55.029
+        },
+        {
+            "lng": -3.89776,
+            "Town": "Achiemore",
+            "Population": 76948,
+            "County": "Highland",
+            "lat": 58.50062
+        },
+        {
+            "lng": -3.26466,
+            "Town": "Abertridwr",
+            "Population": 51055,
+            "County": "Caerphilly",
+            "lat": 51.59752
+        },
+        {
+            "lng": -3.61599,
+            "Town": "Abbotskerswell",
+            "Population": 67099,
+            "County": "Devon",
+            "lat": 50.50507
+        },
+        {
+            "lng": -2.73726,
+            "Town": "Abbey Green",
+            "Population": 89334,
+            "County": "Shropshire",
+            "lat": 52.89665
+        },
+        {
+            "lng": -3.17467,
+            "Town": "Acharn",
+            "Population": 27950,
+            "County": "Angus",
+            "lat": 56.87436
+        },
+        {
+            "lng": 1.25937,
+            "Town": "Alby Hill",
+            "Population": 77004,
+            "County": "Norfolk",
+            "lat": 52.8632
+        },
+        {
+            "lng": -3.16277,
+            "Town": "Abbeyhill",
+            "Population": 104798,
+            "County": "City of Edinburgh",
+            "lat": 55.95803
+        },
+        {
+            "lng": -3.05199,
+            "Town": "Afon Eitha",
+            "Population": 168790,
+            "County": "Wrexham",
+            "lat": 53.00216
+        },
+        {
+            "lng": -1.87097,
+            "Town": "Agglethorpe",
+            "Population": 49126,
+            "County": "North Yorkshire",
+            "lat": 54.27408
+        }
+    ];
 
 
-function d3Draw2() {
-    // Width and height
-    var w = 500;
-    var h = 300;
-    var padding = 20;
+// Width and height
+var BoxWidth = 628.09174;
+var boxHeight = 1051.4788;
+var padding = 20;
+var mapDomain = {
+    "longitudeMin": -10.476361,
+    "longitudeMax": 1.765083,
+    "latitudeMin": 49.162600,
+    "latitudeMax": 60.846142
+};
 
-    //Width = 300;
-    //Height = 200;
+// Create scale functions
+var xScale = d3.scaleLinear()
+    .domain([mapDomain.longitudeMin, mapDomain.longitudeMax])
+    .range([padding, BoxWidth - padding * 2]);
 
-    // Create scale functions
-    var xScale = d3.scaleLinear()
-        .domain([-10, 60])
-        .range([padding, w - padding * 2]);
+var yScale = d3.scaleLinear()
+    .domain([mapDomain.latitudeMin, mapDomain.latitudeMax])
+    .range([boxHeight - padding, padding]);
 
-    var yScale = d3.scaleLinear()
-        .domain([0, 60])
-        .range([h - padding, padding]);
+var aScale = d3.scaleSqrt()
+    .domain([0, d3.max(dataset, function (d) { return d.Population; })])
+    .range([0, 10]);
 
+
+function d3Draw() {
     var aScale = d3.scaleSqrt()
         .domain([0, d3.max(dataset, function (d) { return d.Population; })])
         .range([0, 10]);
@@ -62,8 +119,8 @@ function d3Draw2() {
     // Create SVG element
     var svg = d3.select("body")
         .append("svg")
-        .attr("width", w)
-        .attr("height", h);
+        .attr("width", BoxWidth)
+        .attr("height", boxHeight);
 
     //var svg = d3.select("body").append("svg").attr("width", Width).attr(
     //    "Height", Height);
@@ -80,7 +137,7 @@ function d3Draw2() {
             return (yScale(d.lat));
         })
         .attr("r", function (d) {
-            return 10;
+            return (aScale(d.Population));
         })
         .attr("fill", function (d) {
             return ("rgb(0,255,0)");
@@ -120,4 +177,4 @@ function loadData() {
 }
 
 //window.onload = loadData;
-window.onload = d3Draw2;
+window.onload = d3Draw;
